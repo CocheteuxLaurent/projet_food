d35c0e0a149c35f0229c400d9cf9b17dad0066c0
laure <laurent.cocheteux@hotmail.fr>
2022-05-21 12:21:11 +0200

    new code

--- lib/sqflite/models/RecipeModel.dart
+++ lib/sqflite/models/RecipeModel.dart
@@ -1,22 +1,31 @@
 class Recipe {
   late final int? recip_id;
+  late String? titre;
   late String? img;
   late String? name;
   late String? description;
+  late bool? favoris;
+  late int? nbfavoris;
 
   Recipe(
     this.recip_id,
+    this.titre,
     this.img,
     this.name,
     this.description,
+    this.favoris,
+    this.nbfavoris,
   );
 
   Map<String, dynamic> toMap() {
     var map = <String, dynamic>{
       'recip_id': recip_id,
+      'titre': titre,
       'img': img,
       'name': name,
-      'description': description
+      'description': description,
+      'favoris': favoris,
+      'nbfavoris': nbfavoris
     };
 
     return map;
